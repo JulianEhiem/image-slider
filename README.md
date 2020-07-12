5db593a860b2982ebefe73ed228dd5fee0b007b1
Julian_Ehiem <jules.ehiem@gmail.com>
2020-07-12 13:54:34 -0400

    edited README, added screenshot and fixed typos

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Background-Image-Slider-Project
+# Image-Slider-Project
+
 A small JavaScript application that wires up an image slider to show different images.
+
+![](img/image-slider-project-screenshot.png)
+
+## Demo
+
+Here's a working live demo of the project: https://julianehiem.github.io/image-slider/
+
+## Tech/framework used
+
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [JS](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Links
+
+- Repository:https://github.com/JulianEhiem/image-slider
+
+## License
+
+This project is licensed under the terms of the **MIT** license.
+
+MIT © [JulianEhiem](https://github.com/JulianEhiem)
--- js/app.js
+++ js/app.js
@@ -1,39 +1,4 @@
-// immediate invoked function expression
-
-//(function() {
-//  const pictures = [
-//    "contBcg-0",
-//    "contBcg-1",
-//    "contBcg-2",
-//    "contBcg-3",
-//    "contBcg-4"
-//  ];
-//
-//  //select both left and right button. Add event listeners
-//  const buttons = document.querySelectorAll('.btn')
-//  const imageDIV = document.querySelector('.img-container')
-//  let counter = 0
-//
-//  buttons.forEach(function(button){
-//    button.addEventListener('click', function(e){
-//      if (button.classList.contains('btn-left')){
-//        counter--
-//        if(counter < 0){
-//          counter = pictures.length -1
-//        }
-//        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-//      }
-//      if (button.classList.contains('btn-right')){
-//        counter++
-//        if(counter > pictures.length -1){
-//          counter = 0
-//        }
-//        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-//      }
-//    })
-//  })
-//})();
-
+// Add Images
 const images = [
   "../img/image1-400x400.jpg",
   "../img/image2-400x400.jpg",
@@ -42,6 +7,8 @@ const images = [
   "../img/image5-400x400.jpg",
   "../img/image6-400x400.jpg",
 ];
+
+//DOM Elements
 const leftArrow = document.querySelector(".btn-left");
 const rightArrow = document.querySelector(".btn-right");
 const imgContainer = document.querySelector(".img-container");
